a5effca00b7b22f89b0812a783b8a3b7bd0a2590
Falciron <2159742+Falciron@users.noreply.github.com>
2026-05-31 12:53:56 -0700

    Implement the skipped tests on 10_drink_spec
    I just got a new job as a barista at this drinks place. I can get you
    free drinks, you should come by.

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -91,13 +91,16 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:my_horchata) { described_class.new('horchata') }
 
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(my_horchata.type).to be('horchata')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(my_horchata.ounces).to be(16)
       end
     end
   end
@@ -106,18 +109,22 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:my_chai) { described_class.new('chai') }
 
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(my_chai).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
+      subject(:my_lemonade) { described_class.new('lemonade', 15) }
 
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_lemonade).not_to be_full
       end
     end
   end
