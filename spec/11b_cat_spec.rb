9e5c27b50cb2e2ae47c2a8374a2731c6a9d888ce
Falciron <2159742+Falciron@users.noreply.github.com>
2026-05-31 13:55:45 -0700

    Implement the skipped tests on 11b_cat_spec
    Just a tiny cat doing tiny cat things.

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
+  subject(:small_business) { described_class.new('Small Business', 'Longhair', 'Tabby') }
 
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat has method name shared with other classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(small_business).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(small_business).to be_hiding
   end
 end
